17471d555ee7c28bfafd5b97c3cb7aa0791e8579
Muhammad Saad <saadanwar608@gmail.com>
2019-06-12 12:01:03 +0500

    chnaged gui

--- index.php
+++ index.php
@@ -297,32 +297,9 @@
             </div>
             <!-- #User Info -->
             <!-- Menu -->
-            <div class="menu">
-                <ul class="list">
-                    <li class="header">MAIN NAVIGATION</li>
-                    <li class="active">
-                        <a href="index.php">
-                            <i class="material-icons">home</i>
-                            <span>Home</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="pages/typography.php">
-                            <i class="material-icons">text_fields</i>
-                            <span>Typography</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="pages/helper-classes.php">
-                            <i class="material-icons">layers</i>
-                            <span>Helper Classes</span>
-                        </a>
-                    </li>
-                    
-            
-                    
-                </ul>
-            </div>
+            <?php    
+                include('left_menu.php');    
+           ?>
             <!-- #Menu -->
             <!-- Footer -->
             <div class="legal">
